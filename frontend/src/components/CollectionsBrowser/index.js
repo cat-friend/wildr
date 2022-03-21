254f14694f75585d43fcc1f6e3f440ae46f210d9
cat-friend <denisepli@gmail.com>
2022-03-21 15:45:59 -0400

    adding delete selected feature

--- frontend/src/components/CollectionsBrowser/index.js
+++ frontend/src/components/CollectionsBrowser/index.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import * as collectionActions from "../../store/collections";
@@ -13,6 +13,32 @@ function CollectionsBrowser() {
     const collections = useSelector(state => Object.values(state.collections));
     const currId = useSelector(state => state.session.user.id);
     const isUser = Boolean(currId === +userId);
+    const [collectionsIsCheckedArray, setCollectionsIsCheckedArray] = useState(new Array(collections?.length).fill(false));
+    const collectionsArray = [];
+    let checkedCollections
+    const handleCheckboxOnChange = (index) => {
+        const updatedCheckedState = collectionsIsCheckedArray.map((ele, i) => index === i ? !ele : ele)
+        setChecked(updatedCheckedState)
+        checkedCollections = collectionsArray.filter((ele, i) => {
+            if (updatedCheckedState[i]) {
+                return true
+            };
+            return false;
+        });
+    }
+    const handleDeleteSubmit = (e) => {
+        e.preventDefault();
+        checkedCollections = collectionsArray.filter((ele, i) => {
+            if (updatedCheckedState[i]) {
+                return true
+            };
+            return false;
+        });
+        const payload = {
+            collections: checkedCollections,
+            
+        }
+    }
 
     return (<div>
         <h2>
@@ -21,15 +47,15 @@ function CollectionsBrowser() {
         {isUser && (<div><h2>newCollection</h2><h2>Delete selected</h2></div>)}
         <div className="collections">
             {collections.map((ele, i) => {
+                collectionsArray.push(ele.id);
                 return (
                     <div className="collection-badge" key={`${i}`}>
                         <NavLink to={`/collections/${ele.id}`}>{ele.title}</NavLink>
-                        {isUser && (<h2>edit?</h2>)}
+                        {isUser && (<><h2>edit?</h2><div>checkbox</div></>)}
                     </div>
                 )
             })
             }
-
         </div>
     </div>)
 }
