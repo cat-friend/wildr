c9f44bf221b2a875ba027353b945550c053d86f6
cat-friend <denisepli@gmail.com>
2022-03-23 10:47:13 -0400

    added collections image browser, /collections/:collectionId renders appropriately

---
+++ frontend/src/components/CollectionsDetail/CollectionsImageBrowser.js
@@ -0,0 +1,20 @@
+import { NavLink } from "react-router-dom";
+
+function CollectionsImageBrowser({ images }) {
+    if (images) {
+        return (
+            <div className='browser'><h2>Images</h2>
+                <div className='images'>
+                    {images.map((image) => {
+                        return (
+                            <NavLink to={`/images/${image.id}`} key={image.id}><img src={image.url} key={image.id} alt={image.title} className="thumbnails" /></NavLink>
+                        )
+                    })}
+                </div>
+            </div>)
+    }
+    else
+        return (<h2>This collection doesn't contain any images.</h2>)
+};
+
+export default CollectionsImageBrowser
--- frontend/src/components/CollectionsDetail/index.js
+++ frontend/src/components/CollectionsDetail/index.js
@@ -1,23 +1,35 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { NavLink, useParams } from "react-router-dom";
 import * as collectionActions from "../../store/collections"
 import { getOneUser } from "../../store/users"
+import CollectionsImageBrowser from "./CollectionsImageBrowser";
 
 function CollectionsDetail() {
     const { collectionId } = useParams();
     const dispatch = useDispatch();
     const user = useSelector(state => state.user);
+    const collection = useSelector(state => state.collections);
+    // console.log(collection);
+    const userIdFromCollection = collection?.userId;
     useEffect(() => {
         dispatch(collectionActions.loadOneCollection(collectionId));
-    }, [dispatch, collectionId]);
+        if (userIdFromCollection > 0) dispatch(getOneUser(userIdFromCollection));
+    }, [dispatch, collectionId, userIdFromCollection]);
     const currUserId = useSelector(state => state.session.user.id);
-    const images = useSelector(state => state.collections);
+    const images = Object.entries(collection).length > 0 ? Object.values(collection?.images) : undefined;
+    console.log("images", images);
+    const collectionOwner = useSelector(state => state.user);
+    const isOwner = Boolean(currUserId === userIdFromCollection);
 
     return (
         <div>
-            <h1>IMAGES FOR COLLECTION ID #{`${collectionId}`} GO HERE</h1>
-            <h2></h2>
+            <div>
+                <h1>{`${collection.title}`}</h1>
+                <h2><NavLink to={`/users/${userIdFromCollection}`}>{`${collectionOwner?.username}`}</NavLink></h2>
+                {isOwner && (<h2>EDIT</h2>)}
+            </div>
+            <CollectionsImageBrowser images={images} />
         </div>
     )
 }
