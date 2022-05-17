b5afe8f46df547ce8ee6325ba2ccdb5d5f109f0c
cat-friend <denisepli@gmail.com>
2022-05-17 11:15:38 -0700

    fixed usernames not populating on image detail page

--- frontend/src/components/ImageDetail/index.js
+++ frontend/src/components/ImageDetail/index.js
@@ -34,45 +34,30 @@ const ImageDetailPage = () => {
 
     let imageDetailContent;
 
-    if (!sessionUser) {
+    if (sessionUser) {
+        imageDetailContent = (<>
+            <div className="details-container">
+                <div className="image">
+                    <img src={image?.url} className="image-detail" />
+                </div>
+                <div className="details">
+                    <h2>{image?.title}</h2>
+                    {imageUser && <div><NavLink to={`/users/${userId}`}>{imageUser?.username}</NavLink></div>}
+                    <div>{image?.description}</div>
+                </div>
+                <div className="buttons">
+                    {sessionUser.id === userId &&
+                        <><CRUDImageFormModal modalData={modalData} /></>}
+                </div>
+            </div>
+        </>);
+    }
+    else {
         return (<>
             <h1>You are not authorized to view this page</h1>
             <h2>Please register or log in.</h2>
         </>
         )
-    }
-    else {
-        if (sessionUser) {
-            imageDetailContent = (<>
-                <div className="details-container">
-                    <div className="image">
-                        <img src={image?.url} alt={image?.title} className="image-detail" />
-                    </div>
-                    <div className="details">
-                        <h2>{image?.title}</h2>
-                        <p>{image?.description}</p>
-                    </div>
-                </div>
-            </>);
-        }
-        if (sessionUser.id === userId) {
-            imageDetailContent = (<>
-                <div className="details-container">
-                    <div className="image">
-                        <img src={image?.url} className="image-detail" />
-                    </div>
-                    <div className="details">
-                        <h2>{image?.title}</h2>
-                        {imageUser && <div><NavLink to={`/users/${userId}`}>{imageUser?.username}</NavLink></div>}
-                        <div>{image?.description}</div>
-                    </div>
-                    <div className="buttons">
-                        {sessionUser.id === userId &&
-                            <><CRUDImageFormModal modalData={modalData} /></>}
-                    </div>
-                </div>
-            </>);
-        }
     }
     return (<>{imageDetailContent} <AddToCollection imageId={imageId} /></>);
 }
